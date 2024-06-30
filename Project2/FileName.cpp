b62b44eb7c758fa9e732583456c7b7121d8fe759
DiamondAngel1 <tararukolga78@gmail.com>
2024-06-30 21:10:39 +0300

    Function UserInput

---
+++ Project2/FileName.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS  
+#include <iostream>
+#include <Windows.h>
+#include <string.h>
+using namespace std;
+
+void UserInput(char* user, int max) {
+    cout << "¬вед≥ть р€док: ";
+    cin.getline(user, max);
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int max = 500;
+    char user[max];
+
+    UserInput(user, max);
+
+    return 0;
+}
